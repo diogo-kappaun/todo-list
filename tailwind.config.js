a3caf216507ee1edf0fd8c49048d72736715b778
diogo-kappaun <diogohkappaun@gmail.com>
2024-08-15 15:15:57 -0300

    :lipstick: feat: Add new style configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,38 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: 'Inter, sans-serif',
+      },
+      colors: {
+        product: {
+          blue: {
+            dark: '#1E6F9F',
+            normal: '#4EA8DE',
+          },
+          purple: {
+            dark: '#5E60CE',
+            normal: '#8284FA',
+          },
+        },
+        base: {
+          gray: {
+            100: '#F2F2F2',
+            200: '#D9D9D9',
+            300: '#808080',
+            400: '#333333',
+            500: '#262626',
+            600: '#1A1A1A',
+            700: '#0D0D0D',
+          },
+        },
+        feedback: {
+          danger: '#E25858',
+        },
+      },
+    },
   },
   plugins: [],
 }
